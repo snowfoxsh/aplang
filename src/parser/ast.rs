07c05c732925336f7f174c7f8f070298bb3d1248
Patrick Unick <storm1surge@gmail.com>
2023-10-13 17:10:53 -1000

    Define ast structure
    update ast.rs to define Program and Expr structure

--- src/parser/ast.rs
+++ src/parser/ast.rs
@@ -7,6 +7,77 @@ pub trait AstNode {
     fn spanned<'source>(&self) -> SpannedIter<'source, Token>;
 }
 
-// pub enum Node {
-//
-// }
+pub type Program = Vec<Statement>;
+
+pub enum Statement {
+    Assign(Ident, Expr),
+    Return(Expr),
+    Expr(Expr),
+}
+pub enum Expr {
+    Ident(Ident),
+    Literal(Literal),
+    Prefix(Prefix, Box<Expr>),
+    Infix(Box<Expr>, Infix, Box<Expr>),
+    If {
+        condition: Box<Expr>,
+        consequence: Program,
+        alternative: Option<Program>,
+    },
+    Repeat {
+        count: Box<Expr>,
+        body: Program,
+    },
+    RepeatUntil {
+        condition: Box<Expr>,
+    },
+    ForEach {
+        item: Ident,
+        list: Box<Expr>,
+        body: Program,
+    },
+    Procedure {
+        name: Ident,
+        parameters: Vec<Ident>,
+        body: Program,
+    },
+    ProcedureCall {
+        arguments: Vec<Expr>,
+        procedure: Box<Expr>
+    },
+    List(Vec<Expr>),
+    Index {
+        list: Box<Expr>,
+        index: Box<Expr>,
+    },
+}
+
+pub enum Literal {
+    Number(f64),
+    Bool(bool),
+    String(String),
+}
+
+pub struct Ident(String);
+
+pub enum Prefix {
+    Plus,
+    Minus,
+    Not
+}
+
+pub enum Infix {
+    Plus,
+    Minus,
+    Multiply,
+    Divide,
+    Modulo,
+
+    Equal,
+    NotEqual,
+    LessThan,
+    GreaterThan,
+    LessThanEqual,
+    GreaterThanEqual,
+}
+
