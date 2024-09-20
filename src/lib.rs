e986baee68df7ba1ba9b0657d0ea93f3271e09c1
Patrick Unick <dev_storm@winux.com>
2024-09-20 13:03:03 -0700

    [uni] add library support
    this is an option but not recommended.

---
+++ src/lib.rs
@@ -0,0 +1,22 @@
+#![allow(dead_code, unused_variables)]
+
+//! # Hello
+//! if you are looking to use the interpreter
+//! please install it as a binary.
+//! for more information please see [aplang.org](https://aplang.org).
+//! ---
+//! this for if you want to run ApLang as a library.
+//! although this is not officially supported
+//! i still provide it as an option.
+//! everything is public because of that.
+//! use with care
+//! <3
+
+pub mod interpreter;
+pub mod lexer;
+pub mod parser;
+pub mod standard_library;
+pub mod arguments;
+pub mod aplang;
+
+pub use aplang::*;
