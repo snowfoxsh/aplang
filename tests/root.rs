32e0f017fc2d8d3eb2063ef1903c8dd080b1c1f7
Patrick Unick <dev_storm@winux.com>
2025-02-24 13:25:51 -0800

    [uni] add expression tests

--- tests/root.rs
+++ tests/root.rs
@@ -49,6 +49,123 @@ DISPLAY_NOLN("hello world") $hello world\
 }
 
 #[test]
+fn test_arithmetic_expressions() {
+    smart_test(r#"
+    // Addition
+    sum <- 5 + 3
+    DISPLAY(sum) $8
+
+    // Subtraction
+    difference <- 10 - 4
+    DISPLAY(difference) $6
+
+    // Multiplication
+    product <- 7 * 2
+    DISPLAY(product) $14
+
+    // Division
+    quotient <- 8 / 2
+    DISPLAY(quotient) $4
+
+    // Modulo
+    remainder <- 9 MOD 4
+    DISPLAY(remainder) $1
+    "#);
+}
+
+
+#[test]
+fn test_string_concatenation() {
+    smart_test(r#"
+    greeting <- "Hello, "
+    name <- "World!"
+    full_greeting <- greeting + name
+    DISPLAY(full_greeting) $Hello, World!
+    "#);
+}
+
+#[test]
+fn test_list_concatenation() {
+    smart_test(r#"
+    list1 <- [1, 2, 3]
+    list2 <- [4, 5, 6]
+    combined_list <- list1 + list2
+    DISPLAY(combined_list) $[1, 2, 3, 4, 5, 6]
+    "#);
+}
+
+#[test]
+fn test_function_call_expression() {
+    smart_test(r#"
+    PROCEDURE square(x) {
+        RETURN x * x
+    }
+    result <- square(4)
+    DISPLAY(result) $16
+    
+    DISPLAY(square(2 + 8)) $100
+    "#);
+}
+
+#[test]
+fn test_logical_expressions() {
+    smart_test(r#"
+    a <- TRUE
+    b <- FALSE
+
+    // Logical NOT
+    not_a <- NOT a
+    DISPLAY(not_a) $FALSE
+
+    // Logical AND
+    and_result <- a AND b
+    DISPLAY(and_result) $FALSE
+
+    // Logical OR
+    or_result <- a OR b
+    DISPLAY(or_result) $TRUE
+    "#);
+}
+
+#[test]
+fn test_comparison_expressions() {
+    smart_test(r#"
+    x <- 5
+    y <- 10
+
+    DISPLAY(x == y) $FALSE
+    DISPLAY(x != y) $TRUE
+    DISPLAY(x > y) $FALSE
+    DISPLAY(x < y) $TRUE
+    DISPLAY(x >= y) $FALSE
+    DISPLAY(x <= y) $TRUE
+    "#);
+}
+
+#[test]
+fn test_combined_expressions() {
+    smart_test(r#"
+    a <- 2
+    b <- 3
+    c <- 4
+
+    // Parentheses control the order of operations:
+    result <- (a + b) * c
+    DISPLAY(result) $20
+    "#);
+}
+
+
+
+
+
+
+
+
+
+
+
+#[test]
 fn test_var() {
     smart_test(r#"
 pi <- 3.14
